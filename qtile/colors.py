9e9010ecf07aa32599d2620a8196f5a8d682af54
autom4il <autom4il@archlinux.localdomain>
2024-01-14 21:10:04 +0100

    changed everything

---
+++ qtile/colors.py
@@ -0,0 +1,78 @@
+widget_c = [
+        ### GroupBox ###
+        # Color 00
+        ["#ffffff", "#ffffff"], # active
+        # Color 01
+        ["#02848e", "#02848e"], # inactive
+        # Color 02
+        ["#000000", "#000000"], # background
+        # Color 03
+        ["#000000", "#000000"], # highlight
+        # Color 04
+        ["#f86b09", "#f86b09"], # screen border
+        ### Group Box ###
+
+        ### TextBox 1 ###
+        # Color 05
+        ["#000000", "#000000"], # foreground
+        # Color 06
+        ["#028690", "#028690"], # background
+        ### Textbox 1 ###
+
+        ### CurrentLayoutIcon ###
+        # Color 07
+        ["#028690", "#028690"], # background
+        ### CurrentLayoutIcon ###
+
+        ### TextBox 2 ###
+        # Colors 08
+        ["#028690", "#028690"], # foreground
+        # Colors 09
+        ["#000000", "#000000"], # background
+        ### TextBox 2 ###
+
+        ### TextBox 3 ###
+        # Colors 10
+        ["#000000", "#000000"], # foreground
+        # Colors 11
+        ["#394877", "#394877"], # background
+        
+        ### Systray ###
+        # Colors 12
+        ["#394877", "#394877"], # background        
+        
+        ### TextBox 4 ####
+        # Colors 13
+        ["#394877", "#394877"], # foregorund
+        # Colors 14
+        ["#000000", "#000000"], # background
+
+        ### Clock ###
+        # Colors 15
+        ["#ffffff", "#ffffff"], # foreground
+        # Colors 16
+        ["#000000", "#000000"], # background
+]
+
+# Layout colors
+
+monadtall_c = [
+        # Color 00
+        ["#2596be", "#2596be"], # border focus
+        # color 01
+        ["#ffffff", "#ffffff"] # border normal
+]
+
+floating_c = [
+        # Color 00
+        ["#fcc862", "#fcc862"], # border focus
+        # Color 01
+        ["#ffffff", "#ffffff"] # border normal
+]
+
+maxstack_c = [
+        # Color 00
+        ["#2596be", "#2596be"], # border focus
+        # Color 01
+        ["#ffffff", "#ffffff"] # border normal
+]
--- qtile/config.py
+++ qtile/config.py
@@ -1,81 +1,281 @@
-# Qtile main config
-from libqtile import hook
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.dgroups import simple_key_binder
 from libqtile.utils import guess_terminal
+from libqtile.dgroups import simple_key_binder
+from colors import widget_c, monadtall_c, floating_c, maxstack_c
 
-# Import other libraries
-from bar_config import init_screens
-from layouts_config import init_layouts
-import os
-import subprocess
+dgroups_key_binder = simple_key_binder("mod4")
 
 mod = "mod4"
-dgroups_key_binder = simple_key_binder("mod4")
 terminal = guess_terminal()
 
 browser = "firefox"
 physlock = "physlock -p 'Locked!'"
 
-# Windows behaviour
 follow_mouse_focus = False
 
 keys = [
-    # Applications
-    Key([mod], "Return", lazy.spawn(terminal), desc = "Launch Alacritty"),
-    Key([mod], "r", lazy.spawn("rofi -show drun"), desc = "Launch Rofi"),
-    Key([mod], "b", lazy.spawn(browser), desc = "Launch Firefox"),
+        # Applications
+        Key([mod], "Return", lazy.spawn(terminal), desc = "Launch Alacritty"),
+        Key([mod], "r", lazy.spawn("rofi -show drun"), desc = "Launch Rofi"),
+        Key([mod], "b", lazy.spawn(browser), desc = "Launch Firefox"),
 
-    # Lock session
-    Key([mod, "shift"], "l", lazy.spawn(physlock), desc = "Launch Physlock"),
+        # Lock session
+        Key([mod, "shift"], "l", lazy.spawn(physlock), desc = "Launch Physlock"),
 
-    # Sound
-    Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle")),
-    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -c 0 sset Master 1- unmute")),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -c 0 sset Master 1+ unmute")),
+        # Sound
+        Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle")),
+        Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -c 0 sset Master 1- unmute")),
+        Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -c 0 sset Master 1+ unmute")),
 
-    # Change between layouts
-    Key([mod], "Tab", lazy.next_layout(), desc = "Next layout"),
-    
-    # Change between windows if using Max layout
-    Key([mod], "Right", lazy.layout.next(), desc = "Next window"),
-    Key([mod], "Left", lazy.layout.previous(), desc = "Previous windows"),
+        # Change between layouts
+        Key([mod], "Tab", lazy.next_layout(), desc = "Next layout"),
+        
+        # Change between windows if using Max layout
+        Key([mod], "Right", lazy.layout.next(), desc = "Next window"),
+        Key([mod], "Left", lazy.layout.previous(), desc = "Previous windows"),
     ]
 
 # Groups
 groups = [
-    # MonadTall layout
-    Group("1", layout = "monadtall", label = ""),
-    Group("2", layout = "monadtall", label = ""),
-    Group("3", layout = "max", label = ""),
-    Group("4", layout = "monadtall", label = ""),
-    Group("5", layout = "monadtall", label = ""),
-    Group("6", layout = "monadtall", label = ""),
-    Group("7", layout = "monadtall", label = ""),
+        Group("1", layout = "monadtall", label = "", screen_affinity = 0),
+        Group("2", layout = "monadtall", label = "", screen_affinity = 0),
+        Group("3", layout = "max", label = "", screen_affinity = 0),
+        Group("4", layout = "monadtall", label = "", screen_affinity = 0),
+        Group("5", layout = "monadtall", label = "", screen_affinity = 1),
+        Group("6", layout = "monadtall", label = "", screen_affinity = 1),
+        Group("7", layout = "monadtall", label = "", screen_affinity = 1),
     ]
 
-# Mouse
-mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-        start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-        start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+def init_layouts():
+    layouts = [
+        layout.MonadTall(
+            border_focus = monadtall_c[0],
+            border_normal = monadtall_c[1],
+            margin = 8,
+            border_width = 2,
+        ),
+        layout.Floating(
+            border_focus = floating_c[0],
+            border_normal = floating_c[1],
+            border_width = 2,
+        ),
+        layout.Max(
+            border_focus = maxstack_c[0],
+            border_normal = maxstack_c[1],
+            margin = 8,
+            border_width = 2,
+        ),
+    ]
+    return layouts
+
+def init_widgets_list():
+    groups = ['1', '2', '3', '4', '5', '6', '7']
+    primary_widgets_list = [
+       widget.GroupBox(
+            font="hack", 
+            fontsize = 18, 
+            active = widget_c[0], 
+            inactive = widget_c[1], 
+            background = widget_c[2],
+            highlight_color = widget_c[3],
+            this_current_screen_border = widget_c[4],
+            margin_y = 3, 
+            margin_x = 5, 
+            padding_y = 5, 
+            padding_x = 12,
+            highlight_method = "line", 
+            visible_groups = groups[0:4],
+       ),
+       widget.TextBox(
+            font = "JetBrainsMono Nerd Font Mono",
+            text = "",
+            padding = 0,
+            fontsize = 33,
+            foreground = widget_c[5],
+            background = widget_c[6],
+        ),
+        widget.CurrentLayoutIcon(
+            font = "JetBrainsMono Nerd Font Mono",
+            scale = 0.6,
+            background = widget_c[7],
+        ),
+        widget.CurrentLayout(
+            font = "JetBrainsMono Nerd Font Mono",
+            fontsize = "15",
+            padding = 5,
+            background = widget_c[7],
+        ),
+        widget.TextBox(
+            text = "",
+            fontsize = 33,
+            padding = 0,
+            foreground = widget_c[8],
+            background = widget_c[9],
+        ),
+        widget.Spacer(
+            bar.STRETCH,
+        ),
+        widget.TextBox(
+            text = "",
+            fontsize = 33,
+            padding = 0,
+            foreground = widget_c[10],
+            background = widget_c[11],
+        ),
+        widget.Systray(
+            icon_size = 130,
+            padding = 20,
+            background = widget_c[12]
+            
+        ),
+        widget.TextBox(
+            text = "",
+            fontsize = 33,
+            padding = 0,
+            foreground = widget_c[13],
+            background = widget_c[14],
+        ),
+        widget.CPU(
+            format = " {freq_current}GHz {load_percent}% ",
+            font = "JetBrainsMono Nerd Font Mono",
+            fontsize = 16,
+        ),
+        widget.Memory(
+            format = "  {MemUsed:.0f}{mm}/{MemTotal:.0f}{mm} ",
+            font = "JetBrainsMono Nerd Font Mono",
+            fontsize = 16,
+        ),
+        widget.Wlan(
+            format = "{essid} {percent:2.0%}",
+            font = "JetBrainsMono Nerd Font Mono",
+            fontsize = 16,
+            disconnected_message = "Wifi Disabled"
+        ),
+        widget.Spacer(
+            length = 10,
+        ),
+        widget.Battery(
+            charge_char = '',
+            font = "JetBrainsMono Nerd Font Mono",
+            fontsize = 16,
+            update_interval = 1,
+        ),
+        widget.Clock(
+            format = "%I:%M %p ",
+            font = "JetBrainsMono Nerd Font Mono",
+            fontsize = 15,
+            foreground = widget_c[15],
+            background = widget_c[16],
+        ),
+        widget.Clock(
+            format = "%a %Y-%m-%d",
+            font = "JetBrainsMono Nerd Font Mono",
+            fontsize = 16,
+            foreground = widget_c[15],
+            backgroung = widget_c[16],
+        ),
     ]
 
-# Code specific to running the configuration config.py as the main script or
-# imported as "config"
+    secondary_widgets_list = [
+          widget.GroupBox(
+            font="hack",
+            fontsize = 18,
+            active = widget_c[0],
+            inactive = widget_c[1],
+            background = widget_c[2],
+            highlight_color = widget_c[3],
+            this_current_screen_border = widget_c[4],
+            margin_y = 3,
+            margin_x = 5,
+            padding_y = 5,
+            padding_x = 12,
+            highlight_method = "line",
+            visible_groups = groups[4:7],
+        ),
+        widget.TextBox(
+            font = "JetBrainsMono Nerd Font Mono",
+            text = "",
+            padding = 0,
+            fontsize = 33,
+            foreground = widget_c[5],
+            background = widget_c[6],
+        ),
+        widget.CurrentLayoutIcon(
+            font = "JetBrainsMono Nerd Font Mono",
+            scale = 0.6,
+            background = widget_c[7],
+        ),
+        widget.CurrentLayout(
+            font = "JetBrainsMono Nerd Font Mono",
+            fontsize = "15",
+            padding = 5,
+            background = widget_c[7],
+        ),
+        widget.TextBox(
+            text = "",
+            fontsize = 33,
+            padding = 0,
+            foreground = widget_c[8],
+            background = widget_c[9],
+        ),
+    ]
+    widgets = primary_widgets_list, secondary_widgets_list
+    return widgets
+
+def init_screens():
+    return [
+        Screen(
+               top = bar.Bar(widgets=widgets_list[0], size=30),
+               wallpaper = "~/Pictures/arch_wallpaper.png",
+               wallpaper_mode = "fill"
+        ),
+        Screen(
+               top = bar.Bar(widgets=widgets_list[1], size=30),
+               wallpaper = "~/Pictures/arch_wallpaper.png",
+               wallpaper_mode = "fill"
+        ),
+    ]
 
 if __name__ in ["config", "__main__"]:
-    # Initialize Screens
     screens = init_screens()
-
-    # Initialize layouts
+    widgets_list = init_widgets_list()
     layouts = init_layouts()
 
-    # Floating window layout
-    floating_layout = layouts[1]
+mouse = [
+        Drag([mod], "Button1", lazy.window.set_position_floating(),
+            start=lazy.window.get_position()),
+        Drag([mod], "Button3", lazy.window.set_size_floating(),
+            start=lazy.window.get_size()),
+        Click([mod], "Button2", lazy.window.bring_to_front())
+    ]
 
 @hook.subscribe.startup_once
 def autostart():
